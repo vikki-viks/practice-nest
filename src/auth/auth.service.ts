05f63d54ff13fe8c752e8dad993eb57138653216
vikki-viks <vicka.kudryavtseva@yandex.ru>
2022-11-28 19:50:52 +0300

    add bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
 import { User } from 'src/users/user.entity';
-import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @Inject('USER_REPOSITORY')
-    private userRepository: Repository<User>,
-    private jwtService: JwtService,
-  ) {}
-
-  async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.userRepository.findOne({ where: { username } });
-    if (user?.password === password) {
-      const { password, ...result } = user;
-      return result;
-    }
-    return null;
-  }
+  constructor(private jwtService: JwtService) {}
 
   async login(user: User) {
     const payload = { username: user.username, id: user.id };
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -2,7 +2,7 @@ import { Repository } from 'typeorm';
 import { Injectable, Inject } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
   constructor(
@@ -10,10 +10,24 @@ export class UserService {
     private userRepository: Repository<User>,
   ) {}
 
-  create({ username, password }: CreateUserDto) {
+  async create({ username, password }: CreateUserDto) {
+    const hash = await bcrypt.hash(password, 10);
     return this.userRepository.save({
       username,
-      password,
+      password: hash,
     });
+  }
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userRepository.findOne({ where: { username } });
+    if (!user) {
+      return null;
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
   }
 }
